83f498285b496bd88d1e7db64b037669de424bac
Anshuman Mishra <gamerzp780@gmail.com>
2024-12-28 10:02:39 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import QuoteBox from './QuoteBox';
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteBox />
+    </div>
+  );
+}
+
+export default App;
